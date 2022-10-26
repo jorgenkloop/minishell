0532c88f91373b20300f007a0bdcba7f79f00372
jorgenkloop <kishyanraj@gmail.com>
2022-10-26 11:00:15 +0800

    norm fixed 1

--- builtin2.c
+++ builtin2.c
@@ -2,7 +2,7 @@
 #include "libft/libft.h"
 
 //joins 2 strings with only a \n separating them
-char *ft_strjoin_n(char const *s1, char const *s2)
+char    *ft_strjoin_n(char const *s1, char const *s2)
 {
 	char	*newstr;
 	int		s1_len;
@@ -20,98 +20,98 @@ char *ft_strjoin_n(char const *s1, char const *s2)
 	if (newstr == 0)
 		return (0);
 	ft_strlcpy(newstr, s1, s1_len + 1);
-    newstr[s1_len] = '\n';
+	newstr[s1_len] = '\n';
 	ft_strlcat(newstr + (s1_len + 1), s2, s2_len + 1);
 	return (newstr);
 }
 
-void run_env(t_data data)
+void	run_env(t_data data)
 {
-    int     i;
-    int     fd;
+	int	i;
+	int	fd;
 
-    if (data.cmd->args != NULL && data.cmd->args->s[0] != '\0')
-    {
-        printf("-%s-\n", data.cmd->args->s);
-        mini_perror("Please run without options or arguments\n", 1, 1);
-        return ;
-    }
-    i = -1;
-    fd = 1;
-    while (data.envp[++i] != NULL)
-        ft_putendl_fd(data.envp[i], fd);
-    g_status = 0;
+	if (data.cmd->args != NULL && data.cmd->args->s[0] != '\0')
+	{
+		printf("-%s-\n", data.cmd->args->s);
+		mini_perror("Please run without options or arguments\n", 1, 1);
+		return ;
+	}
+	i = -1;
+	fd = 1;
+	while (data.envp[++i] != NULL)
+		ft_putendl_fd(data.envp[i], fd);
+	g_status = 0;
 }
 
-char    **edit_envp(char **envp, char *var, char *str)
+char	**edit_envp(char **envp, char *var, char *str)
 {
-    char    *new;
-    int     len;
-    int     i;
+	char	*new;
+	int		len;
+	int		i;
 
-    i = -1;
-    len = 0;
-    new = ft_strjoin(var, str);
-    if (str[0] != '\0')
-        len = ft_strlen(var);
-    else
-    {
-        while (var[len] != '=')
-            len++;
-        len++;
-    }
-    while (envp[++i] != NULL)
-    {
-        if (!(ft_strncmp(envp[i], var, len)))
-        {
-            free(envp[i]);
-            envp[i] = ft_strdup(new);
-        }
-    }
-    free(new);
-    return (envp);
+	i = -1;
+	len = 0;
+	new = ft_strjoin(var, str);
+	if (str[0] != '\0')
+		len = ft_strlen(var);
+	else
+	{
+		while (var[len] != '=')
+			len++;
+		len++;
+	}
+	while (envp[++i] != NULL)
+	{
+		if (!(ft_strncmp(envp[i], var, len)))
+		{
+			free(envp[i]);
+			envp[i] = ft_strdup(new);
+		}
+	}
+	free(new);
+	return (envp);
 }
 
-void run_cd(t_cmd *c, char ***envp, int flag)
+void	run_cd(t_cmd *c, char ***envp, int flag)
 {
-    char    *s;
-    char    *pwd;
-    int     pos[2];
-    
-    if (!c->args || (c->args && !ft_strncmp(c->args->s, "~", 1)))
-    {
-        pos[0] = 0;
-        pos[1] = 4;
-        s = get_env("$HOME", *envp, pos);
-        flag = 1;
-    }
-    else
-        s = c->args->s;
-    pwd = getcwd(NULL, 0);
-    *envp = edit_envp(*envp, "OLDPWD=", pwd);
-    if (chdir(s) < 0)
-        mini_perror("No such file or directory\n", 1, 0);
-    free(pwd);
-    pwd = getcwd(NULL, 0);
-    *envp = edit_envp(*envp, "PWD=", pwd);
-    free(pwd);
-    pwd = NULL;
-    if (flag == 1)
-        free(s);
+	char	*s;
+	char	*pwd;
+	int		pos[2];
+	
+	if (!c->args || (c->args && !ft_strncmp(c->args->s, "~", 1)))
+	{
+		pos[0] = 0;
+		pos[1] = 4;
+		s = get_env("$HOME", *envp, pos);
+		flag = 1;
+	}
+	else
+		s = c->args->s;
+	pwd = getcwd(NULL, 0);
+	*envp = edit_envp(*envp, "OLDPWD=", pwd);
+	if (chdir(s) < 0)
+		mini_perror("No such file or directory\n", 1, 0);
+	free(pwd);
+	pwd = getcwd(NULL, 0);
+	*envp = edit_envp(*envp, "PWD=", pwd);
+	free(pwd);
+	pwd = NULL;
+	if (flag == 1)
+		free(s);
 }
 
-void    run_exit(t_data data)
+void	run_exit(t_data data)
 {
-    t_cmd    *s;
-    int     status;
+	t_cmd	*s;
+	int		status;
 
-    s = data.cmd;
-    if (s->args == NULL)
-        exit(1);
-    if (s->args != NULL && s->args->next != NULL)
-        return (mini_perror("exit: too many arguments\n", 2, 0));
-    if (s->args && s->args->s[0] < 48 && s->args->s[0] > 57)
-        exit(2);
-    status = ft_atoi(s->args->s);
-    exit(status);
+	s = data.cmd;
+	if (s->args == NULL)
+		exit(1);
+	if (s->args != NULL && s->args->next != NULL)
+		return (mini_perror("exit: too many arguments\n", 2, 0));
+	if (s->args && s->args->s[0] < 48 && s->args->s[0] > 57)
+		exit(2);
+	status = ft_atoi(s->args->s);
+	exit(status);
 }
